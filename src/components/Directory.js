5a31540d42c5a6d5c1b6bb3cc175d15646be7e1d
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-01 18:09:35 -0700

    starter files

---
+++ src/components/Directory.js
@@ -0,0 +1,27 @@
+// We need to call in a random assortment of employees from the API
+// We need to have a DEFAULT sort (by last name)
+// We need a toggler for sorting by AT LEAST one other attribute of the employees
+
+// https://randomuser.me/ is the API to use here
+
+// Display the directory as a... 
+    // table?
+    // Grid of cards?
+    // Both???
+
+// Use some style, maybe Material UI?
+
+function Directory() {
+    // states here
+    
+    // onload GET API data to be rendered
+
+    return (
+        <>
+            <div>Filters Here</div>
+            <div>Map directory Here</div>
+        </>
+    )
+}
+
+export default Directory
